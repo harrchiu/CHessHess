ea0a4a52ae04312f38ef305a03c4b13b8528f523
HC mbp21 <harrchiu@gmail.com>
2022-07-25 01:03:58 -0400

    resolved albert's resolution

--- src/Human.h
+++ src/Human.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#include 'Player.h' 
-=======
 #include "Player.h" 
->>>>>>> 8f61ef85ec08c75ca705bbded0a016d4157df20d
 
 class Human : public Player {
 
--- src/Move.h
+++ src/Move.h
@@ -1,17 +1,9 @@
 #include <utility>
-<<<<<<< HEAD
-#include 'piece.h'
-=======
 #include "Piece.h"
->>>>>>> 8f61ef85ec08c75ca705bbded0a016d4157df20d
 
 struct Move {
     std::pair<int,int> start;
     std::pair<int,int> end;
     PieceType piece;
     PieceType capturedPiece;
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 8f61ef85ec08c75ca705bbded0a016d4157df20d
